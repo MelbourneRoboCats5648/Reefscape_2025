fa3284db4ee4a2c2557abebed1529dc886f887aa
Mikaela <mikaela1685@gmail.com>
2025-01-12 12:06:28 +1100

    moved some constants that were in the drive module.h into constants.h. (also changed the to inlines idk if that will effect anything.

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -12,31 +12,39 @@
 
 
 namespace OperatorConstants {
-inline constexpr int kDriverControllerPort = 0;
+    inline constexpr int kDriverControllerPort = 0;
 }
 
 namespace DriveConstants {
 
-inline constexpr const int FRONT_LEFT_SPEED_MOTOR_ID = 4;
-inline constexpr const int FRONT_RIGHT_SPEED_MOTOR_ID = 2;
-inline constexpr const int BACK_LEFT_SPEED_MOTOR_ID = 6;
-inline constexpr const int BACK_RIGHT_SPEED_MOTOR_ID = 7;
+    inline constexpr const int FRONT_LEFT_SPEED_MOTOR_ID = 4;
+    inline constexpr const int FRONT_RIGHT_SPEED_MOTOR_ID = 2;
+    inline constexpr const int BACK_LEFT_SPEED_MOTOR_ID = 6;
+    inline constexpr const int BACK_RIGHT_SPEED_MOTOR_ID = 7;
 
-inline constexpr const int FRONT_LEFT_DIRECTION_MOTOR_ID = 3;
-inline constexpr const int FRONT_RIGHT_DIRECTION_MOTOR_ID = 5;
-inline constexpr const int BACK_LEFT_DIRECTION_MOTOR_ID = 1;
-inline constexpr const int BACK_RIGHT_DIRECTION_MOTOR_ID = 8;
+    inline constexpr const int FRONT_LEFT_DIRECTION_MOTOR_ID = 3;
+    inline constexpr const int FRONT_RIGHT_DIRECTION_MOTOR_ID = 5;
+    inline constexpr const int BACK_LEFT_DIRECTION_MOTOR_ID = 1;
+    inline constexpr const int BACK_RIGHT_DIRECTION_MOTOR_ID = 8;
 
-inline constexpr const int FRONT_LEFT_DIRECTION_ENCODER_ID = 9;
-inline constexpr const int FRONT_RIGHT_DIRECTION_ENCODER_ID = 10;
-inline constexpr const int BACK_LEFT_DIRECTION_ENCODER_ID = 12;
-inline constexpr const int BACK_RIGHT_DIRECTION_ENCODER_ID = 11;
+    inline constexpr const int FRONT_LEFT_DIRECTION_ENCODER_ID = 9;
+    inline constexpr const int FRONT_RIGHT_DIRECTION_ENCODER_ID = 10;
+    inline constexpr const int BACK_LEFT_DIRECTION_ENCODER_ID = 12;
+    inline constexpr const int BACK_RIGHT_DIRECTION_ENCODER_ID = 11;
 
-inline constexpr auto kMaxSpeed = 3_mps;
-inline constexpr auto kMaxAcceleration = 3_mps_sq;
-inline constexpr auto kMaxAngularSpeed = 3.142_rad_per_s;
-inline constexpr auto kMaxAngularAcceleration = 3.142_rad_per_s_sq;
+    inline constexpr auto kMaxSpeed = 3_mps;
+    inline constexpr auto kMaxAcceleration = 3_mps_sq;
+    inline constexpr auto kMaxAngularSpeed = 3.142_rad_per_s;
+    inline constexpr auto kMaxAngularAcceleration = 3.142_rad_per_s_sq;
 
-inline constexpr units::second_t kDrivePeriod = frc::TimedRobot::kDefaultPeriod;
+    inline constexpr units::second_t kDrivePeriod = frc::TimedRobot::kDefaultPeriod;
+
+//Module constants defining
+    inline constexpr auto kModuleMaxAngularVelocity =
+        std::numbers::pi * 1_rad_per_s;  // radians per second
+
+    inline constexpr auto kModuleMaxAngularAcceleration =
+        std::numbers::pi * 2_rad_per_s / 1_s;  // radians per second^2
+
 
 } 
