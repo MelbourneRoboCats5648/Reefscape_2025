e9a808e44229e1c9416c6cf01709fdf3774f0ae9
musarrat-mahbub <musarrat@mahbub.mihi>
2025-01-19 16:21:07 +1100

    builds, but only without trapezoid motion profiling

--- src/main/cpp/subsystems/LeftClimbSubsystem.cpp
+++ src/main/cpp/subsystems/LeftClimbSubsystem.cpp
@@ -2,6 +2,48 @@
 
 LeftClimbSubsystem::LeftClimbSubsystem() {
   // Implementation of subsystem constructor goes here.
+  rev::spark::SparkMaxConfig motorConfig;
+  //configuring the encoder:
+  motorConfig.encoder.PositionConversionFactor(1).VelocityConversionFactor(1);
+  //Configure the closed loop controller. We want to make sure we set the feedback sensor as the primary encoder.
+  motorConfig.closedLoop
+      .SetFeedbackSensor(ClosedLoopConfig::FeedbackSensor::kPrimaryEncoder)
+      // Set PID values for position control. We don't need to pass a closed loop slot, as it will default to slot 0.
+      .P(0.1)
+      .I(0)
+      .D(0)
+      .OutputRange(-1, 1);
+   /*
+   * kResetSafeParameters is used to get the SPARK MAX to a known state. This is useful in case the SPARK MAX is replaced.
+   * kPersistParameters is used to ensure the configuration is not lost when the SPARK MAX loses power. This is useful for power cycles that may occur mid-operation.
+   */
+  m_motorController.Configure(motorConfig, SparkBase::ResetMode::kResetSafeParameters,
+                    SparkBase::PersistMode::kPersistParameters);
+// Set the idle mode to brake to stop immediately when reaching a limit
+ motorConfig.SetIdleMode(rev::spark::SparkMaxConfig::IdleMode::kBrake); 
+// Enable limit switches to stop the motor when they are closed
+//only hard switch code in this repo, will add others when organised
+ motorConfig.limitSwitch
+      .ForwardLimitSwitchType(rev::spark::LimitSwitchConfig::Type::kNormallyOpen)
+      .ForwardLimitSwitchEnabled(true)
+      .ReverseLimitSwitchType(rev::spark::LimitSwitchConfig::Type::kNormallyOpen)
+      .ReverseLimitSwitchEnabled(true);
+  // Set the soft limits to stop the motor at -50 and 50 rotations
+  //will alter constants 
+  motorConfig.softLimit
+      .ForwardSoftLimit(50)
+      .ForwardSoftLimitEnabled(true)
+      .ReverseSoftLimit(-50)
+      .ReverseSoftLimitEnabled(true);
+   /* Apply the configuration to the SPARK MAX.
+   * kResetSafeParameters is used to get the SPARK MAX to a known state. This is useful in case the SPARK MAX is replaced. 
+   * kPersistParameters is used to ensure the configuration is not lost when the SPARK MAX loses power. This is useful for power cycles that may occur mid-operation.*/
+  m_motorController.Configure(motorConfig, rev::spark::SparkMax::ResetMode::kResetSafeParameters,
+                    rev::spark::SparkMax::PersistMode::kNoPersistParameters);
+//Encoder:
+  SparkRelativeEncoder m_encoder = m_motorController.GetEncoder(); 
+  // Reset the position to 0 to start within the range of the soft limits
+    m_encoder.SetPosition(0);
 }
 
 frc2::CommandPtr LeftClimbSubsystem::LeftClimbUpCommand() {
@@ -16,6 +58,10 @@ frc2::CommandPtr LeftClimbSubsystem::LeftClimbDownCommand() {
   // Subsystem::RunOnce implicitly requires `this` subsystem.
   return Run([this] {m_motorClimbLeft.Set(leftClimbDownSpeed);})
           .FinallyDo([this]{m_motorClimbLeft.Set(0);});
+}
+
+frc2::CommandPtr LeftClimbSubsystem::LeftClimbCommand(units::turn_t goal) {
+
 }
 
 void LeftClimbSubsystem::Periodic() {
