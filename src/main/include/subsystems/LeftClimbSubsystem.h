e3214c3d015d0ddcfe4f618d6653e962c1c1c1f8
musarrat-mahbub <musarrat@mahbub.mihi>
2025-01-19 14:27:56 +1100

    finished but not building

--- src/main/include/subsystems/LeftClimbSubsystem.h
+++ src/main/include/subsystems/LeftClimbSubsystem.h
@@ -4,13 +4,36 @@
 
 #pragma once
 
+#include <frc/Encoder.h>
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
-#include <frc/Joystick.h>
 #include <frc/motorcontrol/VictorSP.h>
+#include <frc/Joystick.h>
+#include <rev/SparkMax.h>
+#include <rev/config/SparkMaxConfig.h>
+#include <frc/controller/PIDController.h>
+
+#include <units/angle.h>
+#include <units/angular_velocity.h>
+#include <units/angular_acceleration.h>
+#include <rev/config/SparkMaxConfig.h>
+#include <frc2/command/Commands.h>
+
+#include <frc/controller/SimpleMotorFeedforward.h>
+#include <frc/trajectory/TrapezoidProfile.h>
+#include <units/acceleration.h>
+#include <units/length.h>
+#include <units/time.h>
+#include <units/velocity.h>
+#include <units/voltage.h>
+
+using namespace rev::spark;
 
 // PWM Ports
 const int motorClimbLeftPort = 1;
+
+//Motor ID
+const int motorClimbLeftID = 1;
 
 // Speeds
 const double leftClimbUpSpeed = 1.0; //was 0.25
@@ -20,15 +43,35 @@ const double leftClimbDownSpeed = -1.0;
 const int leftUpButton = 5;
 const int leftDownButton = 3;
 
+// Soft Limits
+// Plan to change from example base when limits are decided
+const int  extendSoftLimit = 50;
+const int  retractSoftLimit= -50;
+
+//PID Controller constants
+namespace LeftClimbConstants 
+{
+const double kP = 0.0;
+const double kI = 0.0;
+const double kD = 0.0;
+
+//PID Profile 
+const units::turns_per_second_t maximumVelocity = 1.75_tps;
+const units::turns_per_second_squared_t maximumAccelaration = 0.75_tr_per_s_sq;
+//kDt
+const units::second_t kDt = 20_ms;
+const units::turn_t kGoalThreshold = 0.1_tr;
+}
+
 class LeftClimbSubsystem : public frc2::SubsystemBase {
  public:
   LeftClimbSubsystem();
-
   /**
    * LeftClimb command factory method.
    */
   frc2::CommandPtr LeftClimbUpCommand();
   frc2::CommandPtr LeftClimbDownCommand();
+  frc2::CommandPtr LeftClimbCommand(units::turn_t goal);
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
@@ -44,6 +87,8 @@ class LeftClimbSubsystem : public frc2::SubsystemBase {
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
       frc::VictorSP m_motorClimbLeft{motorClimbLeftPort};
-
+      SparkMax m_motorController{motorClimbLeftID, SparkMax::MotorType::kBrushless};
+      SparkClosedLoopController m_closedLoopController = m_motorController.GetClosedLoopController();
+      SparkRelativeEncoder m_encoder = m_motorController.GetEncoder();
+      frc::TrapezoidProfile<units::turn_t> m_profile{{LeftClimbConstants::maximumVelocity, LeftClimbConstants::maximumAccelaration}};
 };
-
