9784f2321907391d326d3bcc433fd8bf58fa40ea
Kamryn <kamryn@robocats.org>
2025-01-20 16:56:02 +1100

    Added first round of branch 38 changes to right climb subsystem.h. Builds successfully.. will do more

--- src/main/include/subsystems/RightClimbSubsystem.h
+++ src/main/include/subsystems/RightClimbSubsystem.h
@@ -8,7 +8,7 @@
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
 #include <frc/motorcontrol/VictorSP.h>
-#include <frc/Joystick.h>
+//#include <frc/Joystick.h> -taken out
 #include <rev/SparkMax.h>
 #include <frc/controller/PIDController.h>
 #include <units/angle.h>
@@ -53,7 +53,7 @@ const int  retractSoftLimit= -50;
 namespace RightClimbConstants {
 //PID Trapezoidal Controller
 static constexpr units::second_t kDt = 20_ms;
-const units::radian_t kGoalThreshold = 0.1_rad; //part of RightClimbCommand
+const units::meter_t kGoalThreshold = 0.1_m; //part of RightClimbCommand
 
 //PID Controller
 const double kP = 0.0;
@@ -61,8 +61,8 @@ const double kI = 0.0;
 const double kD = 0.0;
 
 //PID Profile] 
-const units:: radians_per_second_t maximumVelocity= 1.75_rad_per_s;
-const units::radians_per_second_squared_t maximumAccelaration = 0.75_rad_per_s_sq;
+const units:: meters_per_second_t maximumVelocity= 1.75_mps;
+const units::meters_per_second_squared_t maximumAccelaration = 0.75_mps_sq;
 //radians can also be used
 /*const units::radians_per_second_t maximumVelocity = 1.75_rad_per_s;
 const units::radians_per_second_squared_t maximumAcceleration = 0.75_rad_per_s_sq; */
@@ -85,7 +85,7 @@ class RightClimbSubsystem : public frc2::SubsystemBase {
    */
 
   [[nodiscard]]
-  frc2::CommandPtr RightClimbCommand(units::radian_t goal);
+  frc2::CommandPtr RightClimbCommand(units::meter_t goal);
 
 
 
@@ -121,13 +121,13 @@ class RightClimbSubsystem : public frc2::SubsystemBase {
   // Create a motion profile with the given maximum velocity and maximum
   // acceleration constraints for the next setpoint.
   //frc::TrapezoidProfile<units::meters> m_profile{{1.75_mps, 0.75_mps_sq}};
-  frc::TrapezoidProfile<units::meters>::State m_goal;
-  frc::TrapezoidProfile<units::meters>::State m_setpoint;
+  frc::TrapezoidProfile<units::meters> m_profile{{RightClimbConstants::maximumVelocity, RightClimbConstants::maximumAccelaration}};
+  frc::TrapezoidProfile<units::meters>::State m_rightClimbGoal;
+  frc::TrapezoidProfile<units::meters>::State m_rightClimbSetpoint;
   SparkClosedLoopController m_closedLoopController = m_motor.GetClosedLoopController();
 
   //Edited Trapezoid Profiler with turn_t doesn't build
   // Create a motion profile with the given maximum velocity and maximum
   // acceleration constraints for the next setpoint.
-  //frc::TrapezoidProfile<units::radian_t> m_profile{{RightClimbConstants::maximumVelocity, RightClimbConstants::maximumAccelaration}};
   // turns can also be changed to radians like the constants
 };
