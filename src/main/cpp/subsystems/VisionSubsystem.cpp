358c9cff82eb268663217112dd1a79e4220c6858
Mikaela <mikaela1685@gmail.com>
2025-05-18 14:20:53 +1000

    no message

--- src/main/cpp/subsystems/VisionSubsystem.cpp
+++ src/main/cpp/subsystems/VisionSubsystem.cpp
@@ -1,3 +1,11 @@
 #include "subsystems/VisionSubsystem.h"
 
 VisionSubsystem::VisionSubsystem() {}
+
+void VisionSubsystem::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
+void VisionSubsystem::SimulationPeriodic() {
+  // Implementation of subsystem simulation periodic method goes here.
+}
