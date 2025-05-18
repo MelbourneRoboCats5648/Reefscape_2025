afb3d8cf3d55781c2475da2b335e818a3ee7a20a
Mikaela <mikaela1685@gmail.com>
2025-05-18 15:07:25 +1000

    added stuff to drivesusbsytme

--- src/main/include/subsystems/DriveSubsystem.h
+++ src/main/include/subsystems/DriveSubsystem.h
@@ -26,6 +26,15 @@
 
 #include <ctre/phoenix6/Pigeon2.hpp>
 
+//Odometry 
+#include <frc/estimator/PoseEstimator.h>
+#include <frc/estimator/SwerveDrivePoseEstimator.h>
+#include <frc/geometry/Pose2d.h>
+
+#include <wpi/array.h>
+
+//PhotonVision
+
 using namespace ctre::phoenix6::hardware;
 
 using namespace DriveConstants;
@@ -60,6 +69,8 @@ class DriveSubsystem : public frc2::SubsystemBase {
                            units::second_t period = DriveConstants::kDrivePeriod);
 
     void SetModuleStates(wpi::array<frc::SwerveModuleState, 4> desiredStates);
+    frc::ChassisSpeeds GetRobotRelativeSpeeds();
+
     void StopAllModules();
     frc2::CommandPtr StopCommand();
     frc2::CommandPtr SmartDashboardOutputCommand();
@@ -68,6 +79,23 @@ class DriveSubsystem : public frc2::SubsystemBase {
     frc2::CommandPtr SwitchRobotRelativeCommand();
     frc2::CommandPtr SwitchFieldRelativeCommand();
     frc2::CommandPtr ToggleFieldRelativeCommand();
+
+      //void UpdateOdometry(frc::Pose2d m_pose);
+
+      /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
+      frc::Pose2d GetPosition();
+
+        /**
+     * Resets the odometry to the specified pose.
+     *
+     * @param pose The pose to which to set the odometry.
+     */
+      void ResetPosition(frc::Pose2d pose);
+      void SetPositionToZeroDistance();
 
  private:
     //Gyro
@@ -93,6 +121,16 @@ class DriveSubsystem : public frc2::SubsystemBase {
     nt::StructPublisher<frc::Rotation2d> m_fieldHeadingPublisher;
 
     units::degree_t m_fieldGyroOffset = 0.0_deg;
+
+    // PoseEstimator class for tracking robot pose
+    // 4 defines the number of modules
+    frc::SwerveDrivePoseEstimator<4> m_poseEstimator{kinematics, frc::Rotation2d{GetHeading()},
+                                {m_frontLeftModule.GetPosition(), m_frontRightModule.GetPosition(),
+                                  m_backLeftModule.GetPosition(), m_backRightModule.GetPosition()}, 
+                                   frc::Pose2d{}, 
+                                  // ( double 0.5, double 0.5, units::radian_t(1)), 
+                                  // ( double 0.5, double 0.5, units::radian_t(1))
+                                  };
                                             
   public:
   frc::SlewRateLimiter<units::meters_per_second> m_xLimiter{kSlewRateTranslation};
