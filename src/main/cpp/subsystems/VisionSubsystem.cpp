ff4eca30d4dea7923ffcf3c36e916f11adc964d0
Mikaela <mikaela1685@gmail.com>
2025-06-22 13:56:05 +1000

    made an aim and range function based on photonvision exampel

--- src/main/cpp/subsystems/VisionSubsystem.cpp
+++ src/main/cpp/subsystems/VisionSubsystem.cpp
@@ -25,6 +25,62 @@ void VisionSubsystem::Periodic() {
   }
 }
 
+void VisionSubsystem::AimAndRange() {
+  //so this will be called by a button.
+  bool targetVisible = false;
+  units::degree_t targetYaw = 0.0_deg;
+  units::meter_t targetRange = 0.0_m;
+  std::set<int> aprilTagSet = {1, 2, 3, 4, 5}; // to be changed
+
+  auto results = camera.GetAllUnreadResults();
+  if (results.size() > 0) 
+  {
+    // Camera processed a new frame since last
+    // Get the last one in the list.
+    auto result = results[results.size() - 1];
+    if (result.HasTargets()) 
+    {
+      // At least one AprilTag was seen by the camera
+      //for (auto& target : result.GetTargets()) 
+      auto target = result.GetBestTarget();
+      //{
+        if (aprilTagSet.contains(target.GetFiducialId())) 
+        {
+          // Found Tag in the targeted apriltage set, record its information
+          targetYaw = units::degree_t{target.GetYaw()};
+          targetRange = photon::PhotonUtils::CalculateDistanceToTarget(
+              0.5_m,      // height of camera
+              1.435_m,    // height of april tage
+              -30.0_deg,  // camera pitch angle
+              units::degree_t{target.GetPitch()});
+          targetVisible = true;
+        }
+      //}
+    }
+  }
+  units::meters_per_second_t xSpeed = 0_mps;
+  units::meters_per_second_t ySpeed = 0_mps;
+  units::radians_per_second_t rotSpeed = 0_rad_per_s;
+
+    // Auto-align
+  if (targetVisible) 
+  {
+    // Driver wants auto-alignment to tag x
+    // And, tag x is in sight, so we can turn toward it.
+    // Override the driver's turn command with an automatic one that turns
+    // toward the tag and gets the range right.
+    rotSpeed = (reefDesiredAngle - targetYaw).value() * visionTurnKP *
+           DriveConstants::kMaxAngularSpeed;
+    xSpeed = (reefDesiredRange - targetRange).value() * visionStrafeKP *
+              DriveConstants::kMaxSpeed; //FIXME CHECK THIS IS RIGHT CONSTANT
+  }
+
+  // Command drivetrain motors based on target speeds
+ 
+  m_drive.Drive(xSpeed, ySpeed, rotSpeed);
+
+}
+
 void VisionSubsystem::SimulationPeriodic() {
   // Implementation of subsystem simulation periodic method goes here.
 }
--- src/main/include/subsystems/VisionSubsystem.h
+++ src/main/include/subsystems/VisionSubsystem.h
@@ -11,6 +11,9 @@
 
 #include "subsystems/DriveSubsystem.h"
 
+#include <photon/PhotonUtils.h>
+#include <set>
+
 class VisionSubsystem : public frc2::SubsystemBase {
   public:
   VisionSubsystem(DriveSubsystem& driveSub);
@@ -18,16 +21,27 @@ class VisionSubsystem : public frc2::SubsystemBase {
   //Will be called periodically whenever the CommandScheduler runs.
   void Periodic() override;
 
+  void AimAndRange();
+
   //Will be called periodically whenever the CommandScheduler runs during simulation.
   void SimulationPeriodic() override;
 
+
   private:
     photon::PhotonCamera camera{"photonvision"}; //check this name is what we gave it on the UI
-    photon::PhotonPoseEstimator photonEstimator{
+    photon::PhotonPoseEstimator photonEstimator
+    {
       VisionConstants::kTagLayout,
       photon::PoseStrategy::MULTI_TAG_PNP_ON_COPROCESSOR,
-      VisionConstants::kRobotToCam};
+      VisionConstants::kRobotToCam //FIX ME CHECK THE ROBOT TO CAM MEASUREMENTS
+    };
     photon::PhotonPipelineResult m_latestResult;
 
     DriveSubsystem& m_drive;
+
+  //FIX THESE CONSTANTS AND MOVE TO CONSTANTS.H AFTER (when ove change to inline)
+  static constexpr double visionTurnKP = 0.01; 
+  static constexpr auto reefDesiredAngle = 0.0_deg;
+  static constexpr double visionStrafeKP = 0.5;
+  static constexpr units::meter_t reefDesiredRange = 1.25_m;
 };
