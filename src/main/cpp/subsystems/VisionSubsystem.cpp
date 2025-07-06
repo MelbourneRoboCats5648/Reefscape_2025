37d379b49afee1ea1af02eaf7f44eb14210172ee
robocats <robocats@robocats.org>
2025-07-06 14:26:07 +1000

    removed vinh's changes. Builds.

--- src/main/cpp/subsystems/VisionSubsystem.cpp
+++ src/main/cpp/subsystems/VisionSubsystem.cpp
@@ -110,9 +110,7 @@ void VisionSubsystem::AimAndRange() {
 std::optional<frc::Pose2d> VisionSubsystem::GetPoseAtTag(const int& reefTagID) {
   if (poseMap.contains(reefTagID)) {
     std::cout << "ReefTagId:" << reefTagID << " has Pose2D" << std::endl;
-
-    if (!poseMap.contains(reefTagID)) return {};
-
+    
     std::cout << "X at" << reefTagID << "=" << poseMap.at(reefTagID).X().value() << std::endl;
     std::cout << "Y at" << reefTagID << "=" << poseMap.at(reefTagID).Y().value() << std::endl;
     std::cout << "Z Rot at" << reefTagID << "=" << poseMap.at(reefTagID).Rotation().Degrees().value() << std::endl;
